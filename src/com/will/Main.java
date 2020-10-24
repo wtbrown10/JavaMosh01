e42bc391248f7baa9f43fe2d4f91555f2fc02000
Will brown <wtbrown10@gmail.com>
2020-10-24 01:46:28 -0400

    added calculateWage, set/getBaseSalary, set/getHourlyRate

--- src/com/will/Main.java
+++ src/com/will/Main.java
@@ -4,23 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         var employee = new empyloyee();
-        employee.baseSalary = 50_0000;
-        employee.extraHours = 20;
+        employee.setBaseSalary(50_000);
+        employee.setHourlyRate(20);
+        int wage = employee.calculateWage(10);
 
-
-        int baseSalary = 50_000;
-        int extraHours = 10;
-        int hourlyRate = 20;
-
-        int wage = calculateWage(baseSalary, extraHours, hourlyRate);
         System.out.println("$" + wage);
     }
 
-    public static int calculateWage(
-            int baseSalary,
-            int extraHours,
-            int hourlyRate
-    ) {
-        return baseSalary + (extraHours * hourlyRate);
-    }
+
 }
--- src/com/will/empyloyee.java
+++ src/com/will/empyloyee.java
@@ -2,10 +2,30 @@ package com.will;
 
 public class empyloyee {
     public int baseSalary;
-    public int hourlyRate;
+    private int hourlyRate;
     public int extraHours;
 
     public int calculateWage(int extraHours) {
-        return baseSalary + (hourlyRate * extraHours);
+        return baseSalary + (getHourlyRate() * extraHours);
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less!");
+            this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly rate cannot be less then zero");
+        this.hourlyRate = hourlyRate;
     }
 }
