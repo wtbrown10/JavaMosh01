eac6df273cce011817977a3e3c21dfbb676046c4
Will brown <wtbrown10@gmail.com>
2020-10-26 15:27:57 -0400

    static

--- src/com/will/Employee.java
+++ src/com/will/Employee.java
@@ -4,14 +4,23 @@ public class Employee {
     private int baseSalary;
     private int hourlyRate;
 
+    public static int numberOfEmployees;
+
     public Employee(int baseSalary, int hourlyRate) {
         setBaseSalary(baseSalary);
         setHourlyRate(hourlyRate);
+        numberOfEmployees++;
+    }
 
+    public static void printNumberOfEmployees() {
+        System.out.println(numberOfEmployees);
     }
 
     public int calculateWage(int extraHours) {
         return baseSalary + (getHourlyRate() * extraHours);
+    }
+  public int calculateWage() {
+        return calculateWage(0);
     }
 
     private void setBaseSalary(int baseSalary) {
--- src/com/will/Main.java
+++ src/com/will/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         var employee = new Employee(50_000, 20);
-        int wage = employee.calculateWage(10);
+        Employee.printNumberOfEmployees();
+        int wage = employee.calculateWage();
 
        System.out.println("$" + wage);
 
